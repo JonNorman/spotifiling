c6d8fd5a81f3470c6734b856be1903582d87c585
Jon Norman <jon.norman@depop.com>
2026-02-05 17:42:14 +0000

    feat: remove HTTPs – spotify allows unsecured loopback redirect addresses.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
